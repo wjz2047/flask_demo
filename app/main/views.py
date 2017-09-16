a2c1f9a7b172b609bf0439d57551efcd4f8fa074
weijiazhen <wjz2047@163.com>
2017-09-16 17:09:15 +0800

    add blueprint main

---
+++ app/main/views.py
@@ -0,0 +1,14 @@
+from flask import request, jsonify
+from . import main
+
+@main.route('/v1/main/get/<cust_id>', methods = ['GET'])
+def test_get(cust_id):
+    print('cust_id: %s, type: %s' % (cust_id, type(cust_id)))
+    key1 = request.args.get('key1')
+    print('key1:', key1, 'type:', type(key1))
+
+    result = {
+        'status': 'SUCCESS',
+        'data': 'test get'
+    }
+    return jsonify(result)
