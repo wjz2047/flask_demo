0dd8bdc0e889e3a57b63e59688e7a745dcd096fd
weijiazhen <wjz2047@163.com>
2017-09-16 18:54:05 +0800

    test jsonp

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import request, jsonify
 from . import main
+from ..decorator import jsonp
 
 @main.route('/v1/main/get/<cust_id>', methods = ['GET'])
+@jsonp
 def test_get(cust_id):
     print('cust_id: %s, type: %s' % (cust_id, type(cust_id)))
     key1 = request.args.get('key1')
